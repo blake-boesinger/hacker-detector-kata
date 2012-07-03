74ee55dd70bf2e02aab01050a900b9f6faa2bccc
Blake Boesinger <blake@boesinger.org>
2012-07-03 18:48:27 +0100

    reformat code

--- src/test/scala/HackerPolicyTest.scala
+++ src/test/scala/HackerPolicyTest.scala
@@ -7,29 +7,29 @@ import org.junit.Assert._
 class HackerPolicyTest {
 
 
-  val policy : HackerPolicy = new HackerPolicy
+  val policy: HackerPolicy = new HackerPolicy
 
   val sixMinutes = 360000
 
 
   @Test
   def oneFailedLoginWithinLessThanFiveMinutesIsNotHacking {
-          assertFalse(policy.isHacker(1, Date(0), Date(1)))
+    assertFalse(policy.isHacker(1, Date(0), Date(1)))
   }
 
 
   @Test
-    def fiveFailedLoginsWithinLessThanFiveMinutesIsHacking {
-            assertTrue(policy.isHacker(5, Date(0), Date(1)))
-    }
+  def fiveFailedLoginsWithinLessThanFiveMinutesIsHacking {
+    assertTrue(policy.isHacker(5, Date(0), Date(1)))
+  }
 
   @Test
-    def oneFailedLoginInGreaterThanFiveMinutesIsNotHacking {
-            assertFalse(policy.isHacker(1, Date(0), Date(sixMinutes)))
-    }
+  def oneFailedLoginInGreaterThanFiveMinutesIsNotHacking {
+    assertFalse(policy.isHacker(1, Date(0), Date(sixMinutes)))
+  }
 
   @Test
-    def fiveFailedLoginsInGreaterThanFiveMinutesIsNotHacking {
-            assertFalse(policy.isHacker(5, Date(0), Date(sixMinutes)))
-    }
+  def fiveFailedLoginsInGreaterThanFiveMinutesIsNotHacking {
+    assertFalse(policy.isHacker(5, Date(0), Date(sixMinutes)))
+  }
 }
