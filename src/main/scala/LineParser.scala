351eaebe998b8a3e7fed0c9f023eb53bdd519adc
Blake Boesinger <blake@boesinger.org>
2012-07-03 18:47:25 +0100

    don't need the new operator for case classes

--- src/main/scala/LineParser.scala
+++ src/main/scala/LineParser.scala
@@ -13,7 +13,7 @@ class LineParser {
     val tuple = (ip, time, action, username)
 
     tuple match {
-     case Tuple4(ip:String, date:String, action:String,username:String) => Line(new IpAddress(ip), new Date(date.toLong),new Action(action),new Username(username))
+     case Tuple4(ip:String, date:String, action:String,username:String) => Line( IpAddress(ip), Date(date.toLong),Action(action),Username(username))
     }
 
 
